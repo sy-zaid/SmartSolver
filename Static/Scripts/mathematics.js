02ca6cacaaef182a7acb377b472e3fd864a83768
SyedZaid <syedmzaid.99@gmail.com>
2023-12-24 05:53:09 +0500

    Added hide one feature functionality

--- Static/Scripts/mathematics.js
+++ Static/Scripts/mathematics.js
@@ -1,7 +1,15 @@
 function clearInput(inputbox_id) {
-
   var inputField = document.getElementById(inputbox_id);
-//   console.log(inputField);
-  inputField.value = '';    
+  //   console.log(inputField);
+  inputField.value = "";
+}
+
+function showSection(sectionId) {
+  // Hide all sections
+  document.getElementById("PA-whole-div-2").classList.add("hidden");
+  document.getElementById("PA-whole-div-3").classList.add("hidden");
   
+
+  // Show the selected section
+  document.getElementById(sectionId).classList.remove("hidden");
 }
