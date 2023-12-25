ae86c4b2a7c45a00041bbcc9de51f2b77cf2179c
sy-zaid <syedmzaid.99@gmail.com>
2023-12-25 15:41:52 +0500

    WIll change the func of lgcmf and mmm

--- Static/Scripts/mathematics.js
+++ Static/Scripts/mathematics.js
@@ -6,10 +6,18 @@ function clearInput(inputbox_id) {
 
 function showSection(sectionId) {
   // Hide all sections
-  document.getElementById("PA-whole-div-2").classList.add("hidden");
-  document.getElementById("PA-whole-div-3").classList.add("hidden");
-  
+  // div1 = document.getElementById("PA-whole-div-2");
+  // div2 = document.getElementById("PA-whole-div-3");
+  document.getElementById("div-out-lgcmf").classList.add("hidden");
+  document.getElementById("div-out-mmm").classList.add("hidden");
+  // div2.classList.add("hidden");
 
   // Show the selected section
   document.getElementById(sectionId).classList.remove("hidden");
+}
+
+// Add this function to submit the form for the active section
+function solveSection(sectionId) {
+  showSection(sectionId);  // Make sure the section is visible
+  document.getElementById("math-form").submit();  // Submit the form
 }
