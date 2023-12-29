74e6c7daf8b8463719f4dbff74be5e56e82f9549
SyedZaid <syedmzaid.99@gmail.com>
2023-12-30 01:45:10 +0500

    Added the JS for algorithms screen

--- Static/Scripts/algorithms.js
+++ Static/Scripts/algorithms.js
@@ -1,3 +1,4 @@
+var lastdynid = "dynamic-input-box";
 function toggleBox() {
   /* Creating function for toggling the 4th input box for algorithms which require more than 3 inputs (e.g. RR requires QT)
     Below are the details for it.
@@ -9,7 +10,7 @@ function toggleBox() {
     - Priority Non-Preemptive - changes
    */
 
-  var section_dynamic_input= document.getElementById("dynamic-input-box");
+  var section_dynamic_input = document.getElementById("input-row-4th");
   var dropdownitem = document.getElementById("algos-dropdown");
 
   if (
@@ -17,10 +18,24 @@ function toggleBox() {
     dropdownitem.value === "SJF-nonpr" ||
     dropdownitem.value === "SJF"
   ) {
-    section_dynamic_input.classList.add("hidden")
-  }
-
-  else if (dropdownitem.value === "RR"){
-
+    section_dynamic_input.classList.add("hidden");
+  } else if (dropdownitem.value === "RR") {
+    section_dynamic_input.classList.remove("hidden");
+    var label = document.getElementsByName("label-dynamic-ib");
+    label[0].textContent = "Enter Quantum Time";
+    var dynamic_input_box = document.getElementById(lastdynid);
+    dynamic_input_box.id = "quantum-time";
+    dynamic_input_box.name = "quantum-time";
+    dynamic_input_box.placeholder = "e.g. 2 secs";
+    lastdynid = "quantum-time";
+  } else if (dropdownitem.value == "Priority" || dropdownitem.value == "Priority-nonpr" ) {
+    section_dynamic_input.classList.remove("hidden");
+    var label = document.getElementsByName("label-dynamic-ib");
+    label[0].textContent = "Enter Priority";
+    var dynamic_input_box = document.getElementById(lastdynid);
+    dynamic_input_box.id = "priority";
+    dynamic_input_box.name = "priority";
+    dynamic_input_box.placeholder = "priority high to low";
+    lastdynid = "priority";
   }
 }
