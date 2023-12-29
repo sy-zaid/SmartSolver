1b77821d4a499958e608d7f761d03a3d7dbf453e
SyedZaid <syedmzaid.99@gmail.com>
2023-12-29 23:33:35 +0500

    Taking a lil break

--- Static/Scripts/algorithms.js
+++ Static/Scripts/algorithms.js
@@ -1,4 +1,26 @@
-// Creating function for toggling the 4th input box for algorithms which require more than 3 inputs (e.g. RR requires QT)
-// function addBox(sectionId){
-//     var 
-// }
+function toggleBox() {
+  /* Creating function for toggling the 4th input box for algorithms which require more than 3 inputs (e.g. RR requires QT)
+    Below are the details for it.
+    - FCFS - stays same
+    - SJF Preemptive - stays same
+    - SJF Non-Preemptive - stays same
+    - Round-Robin - changes
+    - Priority Preemptive - changes
+    - Priority Non-Preemptive - changes
+   */
+
+  var section_dynamic_input= document.getElementById("dynamic-input-box");
+  var dropdownitem = document.getElementById("algos-dropdown");
+
+  if (
+    dropdownitem.value === "FCFS" ||
+    dropdownitem.value === "SJF-nonpr" ||
+    dropdownitem.value === "SJF"
+  ) {
+    section_dynamic_input.classList.add("hidden")
+  }
+
+  else if (dropdownitem.value === "RR"){
+
+  }
+}
